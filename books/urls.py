e9285a40a668500385ccc7ba4c18c39aff0533e0
Yaroslav Boiko <yaroslav.boyko.2311@gmail.com>
2022-11-24 17:43:21 +0200

    ListView

--- books/urls.py
+++ books/urls.py
@@ -1,17 +1,20 @@
 from django.urls import path
 
-from .views import all_author, all_books, all_publisher, all_store, book, one_author, one_publisher, store
+from .views import AddAuthor, AllAuthor, DelAuthor, OneAuthor, UpdateAuthor, all_books, all_publisher, all_store, book, one_publisher, store
 
 
 app_name = 'books'
 urlpatterns = [
     path('book', all_books, name='all_books'),
-    path('author', all_author, name='all_author'),
+    path('author', AllAuthor.as_view(), name='all_author'),
+    path('author/create', AddAuthor.as_view(), name='add_author'),
+    path('author/<int:pk>/update', UpdateAuthor.as_view(), name='up_author'),
+    path('author/<int:pk>/delete', DelAuthor.as_view(), name='delauthor'),
     path('publisher', all_publisher, name='all_publisher'),
     path('store', all_store, name='all_store'),
     path('book/<int:pk>', book, name='book'),
     path('publisher/<int:pk>', one_publisher, name='one_publisher'),
-    path('author/<int:pk>', one_author, name='author'),
+    path('author/<int:pk>', OneAuthor.as_view(), name='author'),
     path('store/<int:pk>', store, name='store'),
 
 ]
--- books/views.py
+++ books/views.py
@@ -1,6 +1,9 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.db.models import Avg, Count, Max
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DeleteView, DetailView, ListView, UpdateView
 
 from .models import Author, Book, Publisher, Store
 
@@ -12,14 +15,46 @@ def all_books(request):
     return render(request, 'books/all_books.html', {'all_book': all_book, 'max_price': max_price, 'avg_rat': avg_rat})
 
 
-def all_author(request):
-    all_authors = Author.objects.prefetch_related('book_set__authors').all()
-    return render(request, 'books/all_author.html', {'all_authors': all_authors})
+# def all_author(request):
+#     all_authors = Author.objects.prefetch_related('book_set__authors').all()
+#     return render(request, 'books/all_author.html', {'all_authors': all_authors})
+class AllAuthor(ListView):
+    model = Author
+    template_name = 'books/all_author.html'
+    # all_authors = Author.objects.prefetch_related('book_set__authors').all()
+    context_object_name = 'all_authors'
+    extra_context = {'title': 'List of all authors'}
+    paginate_by = 5
 
 
-def one_author(request, pk):
-    obj = get_object_or_404(Author, pk=pk)
-    return render(request, 'books/one_author.html', {'obj': obj})
+class AddAuthor(LoginRequiredMixin, CreateView):
+    model = Author
+    fields = ['name', 'age']
+    template_name = 'books/create_author.html'
+    pk_url_kwarg = 'pk'
+    success_url = reverse_lazy('books:all_author')
+
+
+class UpdateAuthor(LoginRequiredMixin, UpdateView):
+    model = Author
+    fields = ['name', 'age']
+    template_name = 'books/update_author.html'
+    # pk_url_kwarg = 'pk'
+    # success_url = reverse_lazy('books:author', kwargs={'pk': pk_url_kwarg})
+    success_url = reverse_lazy('books:all_author')
+
+
+class DelAuthor(LoginRequiredMixin, DeleteView):
+    model = Author
+    template_name = 'books/delete_author.html'
+    # success_url = '/books/author'
+    success_url = reverse_lazy('books:all_author')
+
+
+class OneAuthor(DetailView):
+    model = Author
+    template_name = 'books/one_author.html'
+    context_object_name = 'context'
 
 
 def all_publisher(request):
